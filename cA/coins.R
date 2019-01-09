d0a5421a7b560c59dad5889ffd390280784e81be
ben-domingue <ben.domingue@gmail.com>
2019-01-08 19:25:39 -0800

    coins

--- cA/coins.R
+++ cA/coins.R
@@ -2,27 +2,32 @@
 
 ll<-function(p,x) { 
     like<-function(x,p) p^x*(1-p)^(1-x) #this is the likelihood for a single coin toss (the result is in x) where the probability that x=1 is p
-    like<-Vectorize(like,"x")
+    like<-Vectorize(like,"x") #this takes a function of a scalar and converts it to a function of a vector (where each element of the vector will be operated on as in the original function)
     sum(log(like(x,p)))
 }
 
-true.p<-.7
+true.p<-.7 
 
-par(mfrow=c(5,1),mar=c(3,3,1,1),mgp=c(2,1,0))
-ii.seq<-c(1,10,100,1000,10000)
+##the goal is to see how the likelihood behaves as a function of the data and different choices for the estimator of true.p (which, if we just saw the coin tosses, we wouldn't know)
+par(mfrow=c(3,2),mar=c(3,3,1,1),mgp=c(2,1,0),oma=rep(1,4)) #tuning graphical parameters, don't worry about this
+ii.seq<-c(1,10,100,1000,5000,10000) #this will be the number of tosses of the coin with weight true.p
 for (ii in ii.seq) {
-    plot(NULL,xlim=c(0,1),ylim=c(-1e5,0),xlab="",ylab="ll")
-    p<-seq(0,1,length.out=100)
-    x<-rbinom(ii,1,true.p)
+    p<-seq(0,1,length.out=100) #this is the grid of values that the pare
+    x<-rbinom(ii,1,true.p) #this generated the data by taking ii draws from the appropriate binomial distribution. in this degenerate case of only a single trial, the binomial collapses into a bernoulli distribution (which is what governs the random behavior of coins
     out<-numeric()
-    for (i in 1:length(p)) out[i]<-ll(p[i],x)
+    for (i in 1:length(p)) out[i]<-ll(p[i],x) #this computes the likelihood for our "p" grid of values using the data
+    ##now let's see where the ll is maximal
+    ml.p<-optim(.5,function(p,x) -1*ll(p,x),x=x,method="Brent",lower=0,upper=1)$par
+    ##graphing results, don't worry too much about this.
+    plot(NULL,xlim=c(0,1),ylim=c(-1e4,0),xlab="",ylab="ll")
     mtext(side=1,line=2,paste(ii,"tosses;mean",mean(x)),cex=.8)
-    op<-optim(.5,function(p,x) -1*ll(p,x),x=x,method="Brent",lower=0,upper=1)$par
-    lines(p,out)
-    segments(op,-1e5,op,ll(op,x))
     abline(v=true.p,col="red",lty=2)
+    lines(p,out)
+    segments(ml.p,-1e5,ml.p,ll(ml.p,x)) 
 }
 
-##q1-what do you make of the flatness of the likelihood surface? what do you think this corresponds to?
-##q2-how sensitive is "flatness" to the value of true.p?
+##questions that you can explore via adjustments to true.p
+##q1 how does difference between true.p (the vertical dashed red line) and ml.p (the solid vertical black line segment) change as a function of the # of coin flips (ii)
+##q2-what do you make of the flatness of the likelihood surface? what do you think this corresponds to?
+##q3-how sensitive is "flatness" to the value of true.p?
 https://en.wikipedia.org/wiki/Fisher_information
