fcc8e5548f492ebdaaa199c820da41c25bf504fa
ben-domingue <ben.domingue@gmail.com>
2019-01-17 10:52:46 -0800

    cb

--- cB/correlations.R
+++ cB/correlations.R
@@ -12,10 +12,10 @@ get.coors<-function(resp) {
     for (i in 1:ncol(resp)) {
         r.xt[i]<-cor(ss,resp[,i]) #for any i, what is this?
     }
-    for (i in 1:ncol(resp)) {
-        ss<-rowSums(resp[,-i]) #these are the sum scores/observed scores of CTT
-        r.xt[i]<-cor(ss,resp[,i]) #for any i, what is this?
-    }
+    ## for (i in 1:ncol(resp)) {
+    ##     ss<-rowSums(resp[,-i]) #these are the sum scores/observed scores of CTT
+    ##     r.xt[i]<-cor(ss,resp[,i]) #for any i, what is this?
+    ## }
     r.xt
 }
 ##q. why might we be interested in the correlation (r.xt) between an item response and the total score?
@@ -23,11 +23,11 @@ get.coors<-function(resp) {
 plot.fun<-function(resp) { #don't worry too much about the details here in the first pass.
     pv<-colMeans(resp,na.rm=TRUE)
     r.xt<-get.coors(resp)
-    plot(pv,r.xt,pch=19)
+    plot(pv,r.xt,pch=19,cex=2)
     ##everything below here just goes into the red line
     loess(r.xt~pv)->m
     cbind(pv,fitted(m))->tmp
-    lines(tmp[order(tmp[,1]),],col="red")
+    lines(tmp[order(tmp[,1]),],col="red",lwd=3)
     NULL
 }
 plot.fun(resp)
