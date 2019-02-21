e7c34b702b8f9e58cdd18fb3aeb7761fad8581c9
ben-domingue <ben.domingue@gmail.com>
2019-02-21 07:00:47 -0800

    ps4

--- ps4/fit_stat.R
+++ ps4/fit_stat.R
@@ -20,15 +20,15 @@ for (i in 1:1000) {
 
 ##Now let's generate test statistics from a scenario in which the null hypothesis is false (e.g., f() is the student's T distribution with df=5)
 obs<-rt(N,df=5)
-obs.ts1<-sd(obs) #1.4
-obs.ts2<-quantile(obs,.75)-quantile(obs,.25) #1.6
-##so we can see that ts1 falls well outside of the distribution geneated when null hypothesis is true (compare to lefthand panel of figure). ts2, however, is a less sensitive test here (compare to righthand panel of figure). to the extent that we were interested in asking whether the unknown data-generating function f() had variation comparable to the normal, we'd perhaps be better off looking at the SD rather than the IQR.
+obs.ts1<-sd(obs) 
+obs.ts2<-quantile(obs,.75)-quantile(obs,.25) 
 
 par(mfrow=c(1,2))
 plot(density(ts1),xlim=range(c(ts1,obs.ts1)))
 abline(v=obs.ts1,col="red")
 plot(density(ts2),xlim=range(c(ts2,obs.ts2)))
 abline(v=obs.ts2,col="red")
+##so we can see that ts1 falls well outside of the distribution geneated when null hypothesis is true (compare to lefthand panel of figure). ts2, however, is a less sensitive test here (compare to righthand panel of figure). to the extent that we were interested in asking whether the unknown data-generating function f() had variation comparable to the normal, we'd perhaps be better off looking at the SD rather than the IQR.
 
 quantile(ts1,c(.025,.975)) #so, we expect variation in our first test statistic under the null between ~(0.91,1.09)
 quantile(ts2,c(.025,.975)) #here we expect variation between (1.2,1.5)
@@ -39,7 +39,6 @@ get.pseudop<-function(val,ec) {
 }
 get.pseudop(obs.ts1,ts1)
 get.pseudop(obs.ts2,ts2)
-
 ##it looks to me like ts1 is a way better test statistic for examining the hypothesis that observed variation is consistent with what we get from a normal distribution. to be authoritative we'd want to conduct this experiment many many times.
 
 ##1. does ts2 become a more discerning test statistic (in an absolute sense, not relative to ts1) as we increase N?
