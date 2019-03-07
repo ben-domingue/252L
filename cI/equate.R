5391c6805b80188d826ade36a20f145b5b6cb4cb
ben-domingue <ben.domingue@gmail.com>
2019-03-06 16:34:58 -0800

    ci

--- cI/equate.R
+++ cI/equate.R
@@ -1,24 +1,27 @@
+##the goal here is to test a bunch of different equating techniques.
+##note: i'm going to occasionally equate things that *should not* be equated. if you're following closely, you should be able to see this coming.
 
 read.table("https://raw.githubusercontent.com/ben-domingue/252L/master/data/rasch.txt")->resp
 ##we're now going to form a bunch of groups to be equated. make sure you understand what each one is!!! some of them are randomly equivalent. others, not at all!
+##first i'm going to do some ordering with rows and columns, don't worry about this bit.
 sample(1:nrow(resp),replace=FALSE)->index
-resp[index,]->resp
+resp[index,]->resp 
 sample(1:ncol(resp),replace=FALSE)->index
-resp[,index]->resp
+resp[,index]->resp 
 
-pairs<-list( #the first two will be used to define randomly equivalent (re) groups 
-    re.people=list(rowSums(resp[1:500,]),rowSums(resp[501:1000,])), #here we're just taking the first and second half of the data. randomly equivalent due to our reshuffling above.
+pairs<-list( #let's create a variety of different groups
+    re.people=list(rowSums(resp[1:500,]),rowSums(resp[501:1000,])), #here we're just taking the first and second half of the data. randomly equivalent due to our reshuffling above. 
     re.items=list(rowSums(resp[,1:20]),rowSums(resp[,21:50])) #now we're not taking groups of respondents but groups of items! basically creating something like a quasi-parallel form
     )
 ##now let's take systematically varying splits of the data
 resp[,order(colSums(resp))]->resp2
-pairs$diff.items <- list(rowSums(resp2[,1:15]),rowSums(resp2[,16:50])) #again we are grouping by items, but in a very specific way. the first list element is all hard items, the second element all easy items
+pairs$diff.items <- list(rowSums(resp2[,1:15]),rowSums(resp2[,16:50])) #again we are grouping by items, but in a very specific way. the first list element is all hard items, the second element all easy items. so, very different test forms
 resp[order(rowSums(resp)),]->resp2
 pairs$diff.people <- list(rowSums(resp2[1:500,]),rowSums(resp2[501:1000,])) #now we're creating two clearly non-equivalent groups. first half is low-achieving people, second half is high-achieving.
 
 #############################################################################################################################################
 ##linear equating
-linear_equate<-function(nm,L) { ##recal that this is just equating scores equally distant (in SD units) from the mean
+linear_equate<-function(nm,L) { ##recall that this is just equating scores equally distant (in SD units) from the mean
     L[[nm]][[1]]->s1
     L[[nm]][[2]]->s2
     mod<-function(x,y) {
@@ -43,7 +46,7 @@ par(mfrow=c(4,2),mar=c(3,3,1,1),mgp=c(2,1,0))
 lapply(names(pairs),linear_equate,pairs)
 ##let's walk through figures. left hand is map of group 1 scores onto group 2 scale, right hand is reversed.
 ##black dashed line is 45 degree line, red line is the map
-##rows are the different ways of splitting the data from above. which of these is sensible?
+##rows are the different ways of splitting the data from above. which of these equating schemes is sensible?
 
 #############################################################################################################################################
 ##equipercentile equating
@@ -82,6 +85,7 @@ lapply(names(pairs),equipercentile_equate,pairs)
 ##     equate(s1,s2,method="tucker")
 ## }
 
+#############################################################################################################################################
 #############################################################################################################################################
 ##stocking lord
 ##recall that this is the irt-based approach. we're going to minimize differences between test characteristic curves (TCCs) based on common items
