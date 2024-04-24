00ce6656ee064726b9576300422cdfa56fca3d6e
Ben Domingue <ben.domingue@gmail.com>
2024-04-24 13:23:35 -0700

    ps3

--- ps3/eap_versus_mle.R
+++ ps3/eap_versus_mle.R
@@ -14,14 +14,14 @@ a<-exp(rnorm(ni,mean=0,sd=.25)) #discriminations
 c<-runif(ni,min=0,max=0)
 
 th<-rnorm(100)
-est1<-est2<-numeric()
+est.mle<-est.eap<-numeric()
 
 for (i in 1:length(th)) {
     ##we'll use th to simulate data
     p<-get_pr(th[i],a,b,c)
     resp<-rbinom(length(p),1,p)
     ##old MLE estimation based on optimization of f1
-    est1[i]<-optim(0,f1,method='Brent',lower=-10,upper=10,a=a,b=b,c=c,resp=resp)$par
+    est.mle[i]<-optim(0,f1,method='Brent',lower=-10,upper=10,a=a,b=b,c=c,resp=resp)$par
     ##EAP estimation!!
     x<-seq(-5,5,length.out=1000)
     n<-d<-numeric() #numerator and deminator here  ##https://www.rasch.org/rmt/rmt163i.htm
@@ -32,11 +32,11 @@ for (i in 1:length(th)) {
         n[ii]<-x0*dnorm(x0)*p
         d[ii]<-dnorm(x0)*p
     }
-    est2[i]<-sum(n)/sum(d)
+    est.eap[i]<-sum(n)/sum(d)
 }
-df<-data.frame(th=th,est1=est1,est2=est2)
+df<-data.frame(th=th,est.mle=est.mle,est.eap=est.eap)
 plot(df)
-rmse(th,est1)
-rmse(th,est2) ##what do you notice about the RMSEs?
+rmse(th,est.mle)
+rmse(th,est.eap) ##what do you notice about the RMSEs?
 
 
