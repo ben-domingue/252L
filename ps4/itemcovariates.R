1e9d4f112c425c38d43e897692c09394d710e075
ben-domingue <ben.domingue@gmail.com>
2024-05-13 05:33:42 -0700

    ps4

--- ps4/itemcovariates.R
+++ ps4/itemcovariates.R
@@ -3,6 +3,7 @@ nm <- c("frac20")
 df <- dataset$table(nm)$to_data_frame()
 library(lme4)
 
+##A
 ##lme4 style model
 m1<-lmer(resp~0+(1|id)+item,df)
 resp<-irw::long2resp(df)
@@ -16,25 +17,36 @@ z<-merge(fe,co,by=0)
 plot(z[,-1]) #very stylish!
 abline(0,1) #but they depend on different identification assumptions
 
+##B
 ##now let's focus on the Q matrix components
 nms<-paste("Qmatrix__",1:8,sep='')
 fm<-paste("resp~0+(1|id)+",paste(nms,collapse="+"))
 m2<-lmer(as.formula(fm),df)
 
+##C
 ##now a CDM approach
 library(GDINA)
 m3 <- GDINA(frac20$dat,frac20$Q,model="DINA") #you'll need to install GDINA
 coef(m3) #these show the probability of a correct response for a given pattern of skills. you can see the A part of DINA in the fact that the probabilities are constant for patterns that are not all 1s
 z<-personparm(m3) #here we have hard calls for whether a person has each of the 8 skills
-##hard to know how to contrast theta estimates and skills so here are two ad hoc approaches
-##first let's look at ability estimates and average number of skills
+##hard to know how to contrast theta estimates and skills but one question we can ask is about the relationship between skill acquisition and ability. 
+##specifically let's look at ability estimates and average number of skills
 th<-ranef(m1)[[1]][,1]
 plot(th,rowSums(z)) 
-##second let's look at ability estimates for each skill
-th.avg<-numeric()
-for (i in 1:ncol(z)) th.avg[i]<-mean(th[z[,i]==1],na.rm=TRUE)
-th.avg
 
+##now let's take another perspective: what if we modeled having a specific skill as a function of theta
+xl<-range(th)
+plot(NULL,xlim=xl,ylim=0:1,xlab='theta',ylab='Pr(skill=1)')
+for (i in 1:ncol(z)) {
+    m<-glm(z[,i]~th,family='binomial')
+    xx<-seq(xl[1],xl[2],length.out=1000)
+    co<-coef(m)
+    yy<-1/(1+exp(-(co[1]+co[2]*xx)))
+    lines(xx,yy)
+}
+##What do you make of this figure?
+
+##D
 ##fit
 AIC(m1)
 AIC(m2)
--- ps4/ratingscale.R
+++ ps4/ratingscale.R
@@ -4,6 +4,7 @@ df <- dataset$table(nm)$to_data_frame()
 ##cutting it down
 ids<-sample(unique(df$id),10000)
 df<-df[df$id %in% ids,]
+df$resp<-as.numeric(df$resp)
 
 resp<-irw::long2resp(df)
 resp$id<-NULL
@@ -16,19 +17,21 @@ plot(m, type = 'trace')
 coef(m,IRTpars=TRUE)
 ##AA. What do you notice about this model? Focus your attention in particular on the b1-b4 and c paramters. 
 
-##BB. Please describe this model (see "gpcmIRT and rsm" part of the 'IRT models' part of the help page here: https://www.rdocumentation.org/packages/mirt/versions/1.41/topics/mirt).
+##BB. Please describe the key assumption in this model (see "gpcmIRT and rsm" part of the 'IRT models' part of the help page here: https://www.rdocumentation.org/packages/mirt/versions/1.41/topics/mirt).
 
 ##Let's now fit another model
-m0<-mirt(resp,1,'Rasch') #note that we are using a polytomous model we haven't yet talked much about.
+m.pcm<-mirt(resp,1,'Rasch') #here we are fitting the pcm
+plot(m.pcm, type = 'trace')
+coef(m.pcm,IRTpars=TRUE)
 
 ##let's look at both of these. note the AIC, BIC, and number of estimated parameters for each
 m
-m0
-##Note that the AIC and BIC are lower for m0 compared to m. This would conventionally be indicative of better 'fit' for m0 in this case. Let's see what happens when we really reduce the sample size. 
-index<-sample(1:nrow(resp),100)
+m.pcm
+##Note that the AIC and BIC are lower for m.pcm compared to m. This would conventionally be indicative of better 'fit' for m.pcm in this case. Let's see what happens when we really reduce the sample size. 
+index<-sample(1:nrow(resp),200)
 resp.small<-resp[index,]
 m.small<-mirt(resp.small,1,'rsm')
-m0.small<-mirt(resp.small,1,'Rasch') #note that we are using a polytomous model we haven't yet talked much about.
+m.pcm.small<-mirt(resp.small,1,'Rasch')
 m.small
-m0.small
-##CC. How would you think about the difference between the RSM & the PCM in general? This is a test with relatively few items. How might you anticipate a test with 50 (as opposed to 10) items showing sensitivity to the difference between rsm and pcm were we to reduce the number of respondents? 
+m.pcm.small
+##CC. How would you think about the difference between the RSM & the PCM in general? This is a test with relatively few items. How might you anticipate a test with more items (say 50 as opposed to 10) showing sensitivity to the difference between rsm and pcm were we to reduce the number of respondents? 
