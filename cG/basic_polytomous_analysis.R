81edea85b5c4a1f68f48232d0612c5ec982945b1
Ben Domingue <ben.domingue@gmail.com>
2025-01-29 08:38:50 -0800

    Update basic_polytomous_analysis.R

--- cG/basic_polytomous_analysis.R
+++ cG/basic_polytomous_analysis.R
@@ -73,10 +73,12 @@ out<-list()
 for (i in 1:length(mods)) out[[i]]<-f(mods[[i]],n)
 
 par(mfrow=c(2,2),mgp=c(2,1,0),mar=c(3,3,1,1))
+cols<-c("black","red","blue")
 for (i in 1:4) {
-    plot(NULL,xlim=c(-5,5),ylim=0:1,xlab='theta',ylab=paste('Pr(y=',i-1,sep=''))
+    plot(NULL,xlim=c(-5,5),ylim=0:1,xlab='theta',ylab=paste('Pr(y=',i-1,')',sep=''))
     for (j in 1:length(out)) {
         z<-out[[j]]
-        lines(z[[1]],z[[2]][,i])
+        lines(z[[1]],z[[2]][,i],col=cols[j])
     }
 }
+legend("topleft",bty='n',legend=names(mods),fill=cols)
